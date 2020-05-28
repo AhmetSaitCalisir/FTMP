150259cb015c1799409bb0bec6a7c38ac44a8585
AhmetSaitCalisir <56252214+AhmetSaitCalisir@users.noreply.github.com>
2020-05-28 15:44:40 +0300

    v0.0.1
    İlk oluşturulma dosyası

---
+++ app/src/main/java/com/girift/Ftmp/Kullanici.java
@@ -0,0 +1,59 @@
+package com.girift.Ftmp;
+
+public class Kullanici {
+    private String KullaniciID;
+    private String KullaniciAd;
+    private String KullaniciSifre;
+    private String KullaniciBoylam;
+    private String KullaniciEnlem;
+
+    public Kullanici() {}
+
+    public Kullanici(String kullaniciID, String kullaniciAd, String kullaniciSifre, String kullaniciBoylam, String kullaniciEnlem) {
+        KullaniciID = kullaniciID;
+        KullaniciAd = kullaniciAd;
+        KullaniciSifre = kullaniciSifre;
+        KullaniciBoylam = kullaniciBoylam;
+        KullaniciEnlem = kullaniciEnlem;
+    }
+
+    public String getKullaniciID() {
+        return KullaniciID;
+    }
+
+    public void setKullaniciID(String kullaniciID) {
+        KullaniciID = kullaniciID;
+    }
+
+    public String getKullaniciAd() {
+        return KullaniciAd;
+    }
+
+    public void setKullaniciAd(String kullaniciAd) {
+        KullaniciAd = kullaniciAd;
+    }
+
+    public String getKullaniciSifre() {
+        return KullaniciSifre;
+    }
+
+    public void setKullaniciSifre(String kullaniciSifre) {
+        KullaniciSifre = kullaniciSifre;
+    }
+
+    public String getKullaniciBoylam() {
+        return KullaniciBoylam;
+    }
+
+    public void setKullaniciBoylam(String kullaniciBoylam) {
+        KullaniciBoylam = kullaniciBoylam;
+    }
+
+    public String getKullaniciEnlem() {
+        return KullaniciEnlem;
+    }
+
+    public void setKullaniciEnlem(String kullaniciEnlem) {
+        KullaniciEnlem = kullaniciEnlem;
+    }
+}
---
+++ app/src/main/java/com/girift/Ftmp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.girift.Ftmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Bu kısımı deneme ve iki kullanıcı eklemek için yazdım
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Giriş Durumu");
+        myRef.setValue("Çalışıyor");
+        DatabaseReference databaseReference = database.getReference();
+        Kullanici kullanici = new Kullanici("1","Nil","Guresci","38.099763","27.728836");
+        databaseReference.child("Kullanicilar").child(kullanici.getKullaniciID()).setValue(kullanici);
+        Kullanici kullanici1 = new Kullanici("2","Ahmet Sait","Calisir","38.505859","27.706685");
+        databaseReference.child("Kullanicilar").child(kullanici1.getKullaniciID()).setValue(kullanici1);
+    }
+
+    public void GirisYap(View view) {
+        Intent Gecis = new Intent(MainActivity.this,MapsActivity.class);
+        startActivity(Gecis);
+    }
+}
