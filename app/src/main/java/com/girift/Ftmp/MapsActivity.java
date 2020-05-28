9a7fc009a7ffdd53423a93d8f326aba715c8c855
AhmetSaitCalisir <56252214+AhmetSaitCalisir@users.noreply.github.com>
2020-05-28 19:00:04 +0300

    v 0.0.2

--- app/src/main/java/com/girift/Ftmp/MapsActivity.java
+++ app/src/main/java/com/girift/Ftmp/MapsActivity.java
@@ -3,20 +3,42 @@ package com.girift.Ftmp;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private String IDBizim;
+    private String IDGelen;
+    private String EnlemB;
+    private String BoylamB;
+    private String EnlemG;
+    private String BoylamG;
+    private double BoylamO;
+    private double EnlemO;
+    private LatLng Biz;
+    private LatLng Gelen;
+    private LatLng Orta;
+    private MarkerOptions BizM;
+    private MarkerOptions GelenM;
+    private MarkerOptions OrtaM;
+    private Marker Bizz;
+    private Marker Gelenn;
+    private Marker Ortaa;
+    private boolean Bul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,28 +49,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Harita Durumu");
+        DatabaseReference myRef = database.getReference();
+        Biz= new LatLng(0,0);
+        //DENEME
+        IDBizim="1";
+        IDGelen="2";
+        Bul = false;
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                EnlemB = dataSnapshot.child("Kullanicilar").child(IDBizim).getValue(Kullanici.class).getKullaniciEnlem();
+                BoylamB = dataSnapshot.child("Kullanicilar").child(IDBizim).getValue(Kullanici.class).getKullaniciBoylam();
+                EnlemG = dataSnapshot.child("Kullanicilar").child(IDGelen).getValue(Kullanici.class).getKullaniciEnlem();
+                BoylamG = dataSnapshot.child("Kullanicilar").child(IDGelen).getValue(Kullanici.class).getKullaniciBoylam();
+                EnlemO = (Double.parseDouble(EnlemB)+Double.parseDouble(EnlemG))/2;
+                BoylamO = (Double.parseDouble(BoylamB)+Double.parseDouble(BoylamG))/2;
+                Biz = new LatLng(Double.parseDouble(EnlemB),Double.parseDouble(BoylamB));
+                Gelen = new LatLng(Double.parseDouble(EnlemG),Double.parseDouble(BoylamG));
+                Orta = new LatLng(EnlemO,BoylamO);
+                Bizz.remove();
+                BizM.position(Biz);
+                Bizz =  mMap.addMarker(BizM);
+                if (Bul)
+                {
+                    Gelenn.remove();
+                    Ortaa.remove();
+                    GelenM.position(Gelen);
+                    OrtaM.position(Orta);
+                    Gelenn = mMap.addMarker(GelenM);
+                    Ortaa = mMap.addMarker(OrtaM);
+                }
 
-        myRef.setValue("Çalışıyor");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        BizM = new MarkerOptions().position(Biz).title("Siz");
+        Bizz =  mMap.addMarker(BizM);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Biz));
+    }
+
+    public void Bul(View view) {
+
+        GelenM = new MarkerOptions().position(Gelen).title("Buluşulacak Kişi");
+        OrtaM = new  MarkerOptions().position(Orta).title("Orta Nokta");
+        Gelenn = mMap.addMarker(GelenM);
+        Ortaa = mMap.addMarker(OrtaM);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Orta));
+        Bul=true;
     }
 }
